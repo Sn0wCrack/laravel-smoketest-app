241155f1b5fb87e01cf431e8b435fc5d826b384f
Sn0wCrack <442287+Sn0wCrack@users.noreply.github.com>
2026-07-26 14:26:42 +1000

    feat: add ability to set trusted proxies list

---
+++ config/trustedproxy.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+
+    /*
+     * Set trusted proxy IP addresses.
+     *
+     * Both IPv4 and IPv6 addresses are
+     * supported, along with CIDR notation.
+     *
+     * The "*" character is syntactic sugar
+     * within TrustedProxy to trust any proxy
+     * that connects directly to your server,
+     * a requirement when you cannot know the address
+     * of your proxy (e.g. if using ELB or similar).
+     *
+     */
+    'proxies' => Str::of(env('TRUSTED_PROXIES'))
+        ->explode(',')
+        ->toArray(),
+];
